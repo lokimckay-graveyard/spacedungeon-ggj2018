08bb43084407aa07d258500144095b28d3e491e5
Michael Tansell <monkey@tansell.com>
2018-01-27 17:07:02 +1000

    World Block Out Finished
    First pass of the world layout using uniform blocks. These blocks will represent wall and door objects.
    
    Navigation mesh has been baked, and door mesh has navigation obsticle components attached.
    
    Door control is WIP.

---
+++ unity/Assets/Scripts/Env_Doors/DoorLock.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DoorLock : MonoBehaviour {
+
+	// Public variables
+	// Is this door locked by default?
+	public bool locked = false;
+	public string unlockCode = "defaultCode";
+
+	private NavMeshObstacle navModifier;
+
+	void Awake () {
+		navModifier = GetComponent<NavMeshObstacle> ();
+		if (locked) {
+			navModifier.carving = true;
+		} else {
+			navModifier.carving = false;
+		}
+	}
+
+	public bool AttemptUnlock (string code) {
+		if (code == unlockCode) {
+			locked = false;
+			navModifier.carving = false;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void LockDoor () {
+		locked = true;
+		navModifier.carving = true;
+	}
+}
